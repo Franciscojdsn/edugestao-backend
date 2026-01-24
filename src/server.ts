718f7473f8c8c6d4312a1178d345cbe2c240f5fc
Franciscojdsn <franciscojdsn97@gmail.com>
2026-01-24 13:26:18 -0300

    git ignore

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,10 @@ app.get("/health", (req, res) => {
   return res.json({ status: "ok" });
 });
 
+app.get("/users", (req, res) => {
+  return res.json({ status: "users" });
+});
+
 app.listen(3333, () => {
   console.log("🚀 Server running on http://localhost:3333");
 });
